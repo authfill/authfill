7d43eb81e2b12b43e4b0948b57d8948ccc9abef9
Simon Köck <simon@koeck.dev>
2025-06-06 22:38:11 +0200

    feat: add cloudflare pages deployment

--- apps/web/app.config.ts
+++ apps/web/app.config.ts
@@ -3,6 +3,7 @@ import mdx from "@mdx-js/rollup";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "@tanstack/react-start/config";
 import { config } from "dotenv";
+import { cloudflare } from "unenv";
 import alias from "vite-tsconfig-paths";
 
 config({ path: "../../.env" });
@@ -11,5 +12,9 @@ export default defineConfig({
   vite: {
     plugins: [alias(), tailwindcss(), mdx()],
     envPrefix: ["PUBLIC_"],
+  },
+  server: {
+    preset: "cloudflare-pages",
+    unenv: cloudflare,
   },
 });
