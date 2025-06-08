3fa8866107c1a0db26bf13f69df59e35f2f4c7c6
Simon Köck <simon@koeck.dev>
2025-06-08 13:39:06 +0200

    feat: changes

--- apps/web/app/routes/index.tsx
+++ apps/web/app/routes/index.tsx
@@ -10,7 +10,7 @@ function Home() {
     <div className="flex flex-col items-center pt-10 xl:pt-32">
       <Hero />
       <span className="text-center text-lg font-bold text-gray-500">
-        COMING SOON
+        COMING SOON!!!
       </span>
     </div>
   );
