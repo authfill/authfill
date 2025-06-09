0ef81b2617cf506e7fa4faf7e0d8696018a89217
Paul Koeck <paul@koeck.dev>
2025-06-09 19:50:51 +0200

    fix: invalid chrome call instead of browser

--- apps/extension/src/background/utils/notification.ts
+++ apps/extension/src/background/utils/notification.ts
@@ -3,7 +3,7 @@ import browser from "webextension-polyfill";
 export function showNotification(data: { title: string; message: string }) {
   return browser.notifications.create(undefined, {
     type: "basic",
-    iconUrl: chrome.runtime.getURL("icons/icon-dark-128x128.png"),
+    iconUrl: browser.runtime.getURL("icons/icon-dark-128x128.png"),
     ...data,
   });
 }
