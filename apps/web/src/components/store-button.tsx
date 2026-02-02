b8ea5682a79c60a78a3a37e4233bef43dad3aa19
Paul Koeck <paul@koeck.dev>
2026-02-03 00:34:33 +0100

    fix: remove links to opera store

--- apps/web/src/components/store-button.tsx
+++ apps/web/src/components/store-button.tsx
@@ -3,14 +3,12 @@ import { Button, ButtonProps } from "@ui/button";
 import { ChromeIcon } from "@web/components/icons/chrome";
 import { EdgeIcon } from "@web/components/icons/edge";
 import { FirefoxIcon } from "@web/components/icons/firefox";
-import { OperaIcon } from "@web/components/icons/opera";
 import { storeLinks } from "@web/conf/store";
 import { useMemo } from "react";
 import {
-  isEdge,
-  isEdgeChromium,
-  isFirefox,
-  isOpera,
+    isEdge,
+    isEdgeChromium,
+    isFirefox
 } from "react-device-detect";
 
 export function StoreButton(
@@ -18,21 +16,18 @@ export function StoreButton(
 ) {
   const browser = useMemo(() => {
     if (isFirefox && storeLinks["FIREFOX"]) return "FIREFOX";
-    if (isOpera && storeLinks["OPERA"]) return "OPERA";
     if ((isEdge || isEdgeChromium) && storeLinks["EDGE"]) return "EDGE";
     return "CHROME";
   }, []);
 
   const buttonText = useMemo(() => {
     if (browser === "FIREFOX") return "Add to Firefox";
-    if (browser === "OPERA") return "Add to Opera";
     if (browser === "EDGE") return "Add to Edge";
     return "Add to Chrome";
   }, [browser]);
 
   const buttonIcon = useMemo(() => {
     if (browser === "FIREFOX") return <FirefoxIcon />;
-    if (browser === "OPERA") return <OperaIcon />;
     if (browser === "EDGE") return <EdgeIcon />;
     return <ChromeIcon />;
   }, [browser]);
--- apps/web/src/conf/store.ts
+++ apps/web/src/conf/store.ts
@@ -3,7 +3,5 @@ export const storeLinks = {
     "https://chromewebstore.google.com/detail/authfill/doanledhbgobnfeicgdchpilkjkbjddg",
   // FIREFOX: "https://addons.mozilla.org/en-US/firefox/addon/authfill/",
   FIREFOX: "",
-  // OPERA: "https://addons.opera.com/en/extensions/details/authfill/",
-  OPERA: "",
   EDGE: "",
 } as const;
