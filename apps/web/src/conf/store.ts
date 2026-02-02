5696e39fa4f8a8e5601ee0ecc84a1591bd31877e
Paul Koeck <paul@koeck.dev>
2026-02-03 00:35:06 +0100

    fix: update firefox and edge store links

--- apps/web/src/conf/store.ts
+++ apps/web/src/conf/store.ts
@@ -1,7 +1,6 @@
 export const storeLinks = {
   CHROME:
     "https://chromewebstore.google.com/detail/authfill/doanledhbgobnfeicgdchpilkjkbjddg",
-  // FIREFOX: "https://addons.mozilla.org/en-US/firefox/addon/authfill/",
-  FIREFOX: "",
-  EDGE: "",
+  FIREFOX: "https://addons.mozilla.org/en-US/firefox/addon/authfill/",
+  EDGE: "https://microsoftedge.microsoft.com/addons/detail/authfill/acjlnihhamejhmakepblbejadpcficfk",
 } as const;
